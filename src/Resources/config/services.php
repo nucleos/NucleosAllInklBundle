b8599f8c9eaa8fe1d188e445a08f7374d5f2c8a7
core23 <mail@core23.de>
2020-07-03 22:39:12 +0200

    Move configuration to PHP

---
+++ src/Resources/config/services.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * (c) Christian Gripp <mail@core23.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use Nucleos\AllInklBundle\Mailer\Mailer;
+use Nucleos\AllInklBundle\Mailer\SymfonyMailer;
+use Nucleos\AllInklBundle\Service\AccountService;
+use Nucleos\AllInklBundle\Service\AuthService;
+use Nucleos\AllInklBundle\Service\StatisticService;
+use Symfony\Component\DependencyInjection\Parameter;
+use Symfony\Component\DependencyInjection\Reference;
+
+return static function (ContainerConfigurator $container): void {
+    $container->services()
+
+        ->set(SymfonyMailer::class)
+            ->args([
+                new Reference('mailer.mailer'),
+                new Parameter('translator'),
+                [],
+            ])
+
+        ->alias(Mailer::class, SymfonyMailer::class)
+
+        ->set('nucleos_allinkl.service.account', AccountService::class)
+            ->args([
+                new Parameter('nucleos_allinkl.api.api_endpoint'),
+            ])
+
+        ->set('nucleos_allinkl.service.auth', AuthService::class)
+            ->args([
+                new Parameter('nucleos_allinkl.api.api_endpoint'),
+            ])
+
+        ->set('nucleos_allinkl.service.statistic', StatisticService::class)
+            ->args([
+                new Parameter('nucleos_allinkl.api.api_endpoint'),
+            ])
+
+    ;
+};
