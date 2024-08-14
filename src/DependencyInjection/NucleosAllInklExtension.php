387a9da32352dff471b5c13e230add238c9f9a35
core23 <mail@core23.de>
2024-08-14 17:43:13 +0200

    Fix symfony deprecation

--- src/DependencyInjection/NucleosAllInklExtension.php
+++ src/DependencyInjection/NucleosAllInklExtension.php
@@ -14,8 +14,8 @@ namespace Nucleos\AllInklBundle\DependencyInjection;
 use Nucleos\AllInklBundle\Mailer\SymfonyMailer;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 final class NucleosAllInklExtension extends Extension
 {
